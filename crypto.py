0a6cfc4a0a020154cc3d33da8a9f951c1e1604ba
Liam Schumm <liamschumm@icloud.com>
2017-02-06 09:27:42 -0600

    Initial code.

---
+++ crypto.py
@@ -0,0 +1,33 @@
+"""
+[crypto.py]
+
+Cryptography.
+"""
+
+import hashlib
+
+def _md5(string):
+    m = hashlib.md5()
+    m.update(string)
+    return m.digest()
+
+def _sha256(string):
+    s = hashlib.sha256()
+    s.update(string)
+    return s.digest()
+
+def md5(env, args, kwargs):
+    # hashing input
+    if "string" in kwargs && kwargs["string"] in ["t", "true"]:
+        return map(_md5, args)    
+    else: # hashing files
+        return [_md5(open(x).read()) for x in args]
+
+def sha256(env, args, kwargs):
+    # hashing input
+    if "string" in kwargs && kwargs["string"] in ["t", "true"]:
+        return map(_sha256, args)    
+    else: # hashing files
+        return [_sha256(open(x).read()) for x in args]
+
+
