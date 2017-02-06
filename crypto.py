2989cf13bfbb73512693a70cc17bc5037512caa0
Liam Schumm <liamschumm@icloud.com>
2017-02-06 09:35:23 -0600

    Fix code

--- crypto.py
+++ crypto.py
@@ -18,16 +18,19 @@ def _sha256(string):
 
 def md5(env, args, kwargs):
     # hashing input
-    if "string" in kwargs && kwargs["string"] in ["t", "true"]:
+    if "string" in kwargs and kwargs["string"] in ["t", "true"]:
         return map(_md5, args)    
     else: # hashing files
         return [_md5(open(x).read()) for x in args]
 
 def sha256(env, args, kwargs):
     # hashing input
-    if "string" in kwargs && kwargs["string"] in ["t", "true"]:
+    if "string" in kwargs and kwargs["string"] in ["t", "true"]:
         return map(_sha256, args)    
     else: # hashing files
         return [_sha256(open(x).read()) for x in args]
 
 
+verbs = {'md5':md5,
+         'sha256':sha256,
+        }
